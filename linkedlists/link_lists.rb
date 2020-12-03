3bdb402a7624298cd2a5045410b2552197ec73d6
jduong09 <59810054+jduong09@users.noreply.github.com>
2020-12-02 22:34:15 -0500

    Added more features.

--- linkedlists/link_lists.rb
+++ linkedlists/link_lists.rb
@@ -48,28 +48,67 @@ class Linklist
   end
 
   def tail
-    return @tail
+    self.each do |node|
+      if node.next == nil
+        return node
+      end
+    end
   end
 
   def at(index)
-
+    return nil if @head.nil?
+    counter = 0
+    self.each do |node|
+      counter += 1
+      if counter == index
+        return node
+      end
+    end
+    return nil
   end
 
   def pop
-
+    #need the second to last element. Make the second to last item's to be the tail. then we need to remove the last item from the @head.how do we remove instances?
+    return nil if @head.nil?
+    size = self.size
+    second_to_last = size - 1
+    index = 0
+    node = @tail
+    self.each do |node|
+      index += 1
+      if index == second_to_last
+        node.next = nil
+        @tail = node
+      end
+    end
+    return node
   end
 
   def contains?(value)
-
+    return nil if @head.nil?
+    self.each { |node| return true if node.data == value }
+    return false
   end
 
   def find(value)
-
+    return nil if @head.nil?
+    index = 0
+    self.each do |node| 
+      index += 1
+      if value.data == value
+        return index
+      end
+    end
+    return nil
   end
 
   # ( value ) -> ( value ) -> ( value ) -> nil
   def to_s
-
+    string = ""
+    self.each do |node|
+      string += "( #{node.data} ) -> "
+    end
+    return string += "nil"
   end
 
   def each
