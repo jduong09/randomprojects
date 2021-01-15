9cd3b01b36b7088542041af90f61ed5200cc4173
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-01-14 20:21:40 -0500

    Added tests for game_intro

--- tictactoe/spec/tic_tac_toe_spec.rb
+++ tictactoe/spec/tic_tac_toe_spec.rb
@@ -2,43 +2,40 @@
 #Arrange
 #Act
 #Assert
+# To use a message expectation, move 'Assert' before 'Act'.
 
 require_relative "../lib/board.rb"
+require_relative "../lib/player.rb"
 
 describe Board do 
-  #describe "#initialize" do
-    # no need to test because it is just initializing variables
-  #end
-
   describe "#play_game" do
-    # do not need to test this as it is a public script method. 
-    # need to test the methods inside.
-    #describe "#game_intro" do
-      # is a bunch of puts and gets statements.
-      # do not need to test the puts
-      # test the gets methods, so that it returns valid input?
-    #end
+    describe "#game_intro" do
+      subject(:normal_game) { described_class.new }
+      
+      describe "#create_player" do
+        let(:player) { "Justin" }
+        let(:marker) { "x" }
 
-    #describe "#display_board" do
-      # do not understand how to test a loop. 
-    #end
-
-    describe "#game_loop" do 
-
-      subject(:tie_game) { described_class.new() }
-      # test if receive 9 times (9 turns)
-        # test that subject receives take_turn 9 times.
-        it "sends take_turn 9 times" do
-          expect(tie_game).to receive(:take_turn).exactly(9).times
-          tie_game.game_loop
+        it "creates and adds player to @players" do
+          normal_game.create_player(player, marker)
+          expect(normal_game.players.length).to eql(1)
         end
-      # test "#take_turn"
-        # test "#player.move"
-        # test spot_taken?
-      # test "#game_over?"
-        # this is the most important test, and the most complex in this testing suite.
-        # how are we doing to test all 8 possibilities of winning.
-      # test if receive 9 times and not game over, then test if we receive "It's a tie!" message
+      end
+
+      context "when starting the game" do
+        before do
+          player_1 = "Justin"
+          player_2 = "Sang"
+          marker_1 = "x"
+          marker_2 = "o"
+          allow(normal_game).to receive(:player_input).and_return(player_1, marker_1, player_2, marker_2)
+       end
+
+        it "adds 2 players to @players" do 
+          normal_game.game_intro
+          expect(normal_game.players.length).to eql(2)
+        end
+       end
     end
   end
 end
