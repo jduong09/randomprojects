71583139762a365ef611644b1a55fe606473701b
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-01-15 21:13:20 -0500

    Update tic_tac_toe_spec.rb

--- tictactoe/spec/tic_tac_toe_spec.rb
+++ tictactoe/spec/tic_tac_toe_spec.rb
@@ -3,24 +3,25 @@
 #Act
 #Assert
 # To use a message expectation, move 'Assert' before 'Act'.
+# And the golden rule is If its hard to test (specially unit test), maybe your are doing something wrong.
 
 require_relative "../lib/board.rb"
 require_relative "../lib/player.rb"
 
 describe Board do 
+
+  describe "#initialize" do
+    subject(:normal_game) { described_class.new }
+    context "it creates a 3 x 3 array" do
+      it "creates a board with length 3" do 
+        expect(normal_game.board.length).to eql(3)
+      end
+    end
+  end
+
   describe "#play_game" do
     describe "#game_intro" do
       subject(:normal_game) { described_class.new }
-      
-      describe "#create_player" do
-        let(:player) { "Justin" }
-        let(:marker) { "x" }
-
-        it "creates and adds player to @players" do
-          normal_game.create_player(player, marker)
-          expect(normal_game.players.length).to eql(1)
-        end
-      end
 
       context "when starting the game" do
         before do
@@ -35,7 +36,33 @@ describe Board do
           normal_game.game_intro
           expect(normal_game.players.length).to eql(2)
         end
-       end
+
+        describe "#create_player" do
+        let(:player) { "Justin" }
+        let(:marker) { "x" }
+
+          it "creates and adds player to @players" do
+            normal_game.create_player(player, marker)
+            expect(normal_game.players.length).to eql(1)
+          end
+        end
+      end
+    end
+
+    describe "#game_loop" do
+      context "when taking one turn" do
+        describe "#turn_order" do
+          subject(:new_game) { described_class.new() }
+
+          it "returns player 1 if the turn is even" do
+            expect(new_game.turn_order(2)).to eq(0)
+          end
+
+          it "return player 2 if the turn is odd" do
+            expect(new_game.turn_order(3)).to eq(1)
+          end
+        end
+      end
     end
   end
 end
