f6d610991390496ff1efdf7d92c63ba5d70fa5ba
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-02-11 20:57:18 -0500

    Progress on moving pieces on the board

--- chess/lib/board.rb
+++ chess/lib/board.rb
@@ -5,6 +5,11 @@ require_relative "game_pieces/pawn.rb"
 require_relative "game_pieces/queen.rb"
 require_relative "game_pieces/rook.rb"
 
+#2/11/20 schedule
+# handle all the movement of the characters
+# handle check/checkmate
+# game_loop
+
 class Board
   attr_accessor :board
   attr_accessor :gamepieces
@@ -14,8 +19,95 @@ class Board
     @gamepieces = {}
   end
 
-  def move_gamepiece(gamepiece, move)
+  def take_turn(color)
+    puts "It is your turn. Choose the piece you want to move by typing the location of the piece."
+    type = gets.chomp
+    input = gets.chomp
+    col = find_file(input[0])
+    row = find_rank(input[1])
+    
 
+    piece = @gamepieces[color][type].select { |piece| piece.location == input }
+    gamepiece_moves(piece[0], [col, row])
+
+  end
+  #castling. Separate method?
+  def gamepiece_moves(gamepiece, input)
+    all_moves = gamepiece.moves(input)
+    all_moves.map do |move|
+      col = find_file(input[0])
+      row = find_rank(input[1])
+      return col, row
+    end
+    #puts "Where do you want your #{gamepiece.icon} to go?"
+    #user_input = gets.chomp
+    #row = find_rank(input[1])
+    #col = find_file(input[0])
+
+
+  end
+
+  def fill_board
+    @board.each_with_index do |row, idx|
+      row.unshift("#{8 - idx}")
+    end
+    @board.unshift([" ", "a", "b", "c", "d", "e", "f", "g", "h"])
+    create_gamepieces
+    fill_gamepieces
+    display_board
+  end
+
+  def [](col, row)
+    @board[row][col]
+  end
+
+  private
+
+  def find_rank(num) #row
+    @board.each_with_index do |row, idx|
+      if row.first == num
+        return idx
+      end
+    end
+    return "invalid num"
+  end
+
+  def find_file(letter) #col
+    @board[0].index(letter)
+  end
+
+  def fill_gamepieces
+    @gamepieces["white"].each_value do |value|
+      value.each do |piece|
+        change_board(piece.location, piece.icon)
+      end
+    end
+
+    @gamepieces["black"].each_value do |value|
+      value.each do |piece|
+        change_board(piece.location, piece.icon)
+      end
+    end
+  end
+
+  def create_gamepieces
+    @gamepieces["white"] = {
+      "pawn" => [Pawn.new("white", "a2"), Pawn.new("white", "b2"), Pawn.new("white", "c2"), Pawn.new("white", "d2"), Pawn.new("white", "e2"), Pawn.new("white", "f2"), Pawn.new("white", "g2"), Pawn.new("white", "h2")],
+      "knight" => [Knight.new("white", "b1"), Knight.new("white", "g1")],
+      "rook" => [Rook.new("white", "a1"), Rook.new("white", "h1")],
+      "bishop" => [Bishop.new("white", "c1"), Bishop.new("white", "f1")],
+      "king" => [King.new("white")],
+      "queen" => [Queen.new("white")]
+     }
+
+     @gamepieces["black"] = {
+      "pawn" => [Pawn.new("black", "a7"), Pawn.new("black", "b7"), Pawn.new("black", "c7"), Pawn.new("black", "d7"), Pawn.new("black", "e7"), Pawn.new("black", "f7"), Pawn.new("black", "g7"), Pawn.new("black", "h7")],
+      "knight" => [Knight.new("black", "b8"), Knight.new("black", "g8")],
+      "rook" => [Rook.new("black", "a8"), Rook.new("black", "h8")],
+      "bishop" => [Bishop.new("black", "c8"), Bishop.new("black", "f8")],
+      "king" => [King.new("black")],
+      "queen" => [Queen.new("black")]
+     }
   end
 
   def display_board
@@ -30,61 +122,7 @@ class Board
     end
     puts string
   end
-
-  def fill_board
-    @board.each_with_index do |row, idx|
-      row.unshift("#{8 - idx}")
-    end
-    @board.unshift([" ", "a", "b", "c", "d", "e", "f", "g", "h"])
-  end
-
-  def find_file(letter) #col
-    @board[0].index(letter)
-  end
-
-  def find_rank(num) #row
-    @board.each_with_index do |row, idx|
-      if row.first == num
-        return idx
-      end
-    end
-    return "invalid num"
-  end
-
-  def create_gamepieces
-    @gamepieces[:white] = {
-      "pawn" => [Pawn.new("white", "a2"), Pawn.new("white", "b2"), Pawn.new("white", "c2"), Pawn.new("white", "d2"), Pawn.new("white", "e2"), Pawn.new("white", "f2"), Pawn.new("white", "g2"), Pawn.new("white", "h2")],
-      "knight" => [Knight.new("white", "b1"), Knight.new("white", "g1")],
-      "rook" => [Rook.new("white", "a1"), Rook.new("white", "h1")],
-      "bishop" => [Bishop.new("white", "c1"), Bishop.new("white", "f1")],
-      "king" => [King.new("white")],
-      "queen" => [Queen.new("white")]
-     }
-
-     @gamepieces[:black] = {
-      "pawn" => [Pawn.new("black", "a7"), Pawn.new("black", "b7"), Pawn.new("black", "c7"), Pawn.new("black", "d7"), Pawn.new("black", "e7"), Pawn.new("black", "f7"), Pawn.new("black", "g7"), Pawn.new("black", "h7")],
-      "knight" => [Knight.new("black", "b8"), Knight.new("black", "g8")],
-      "rook" => [Rook.new("black", "a8"), Rook.new("black", "h8")],
-      "bishop" => [Bishop.new("black", "c8"), Bishop.new("black", "f8")],
-      "king" => [King.new("black")],
-      "queen" => [Queen.new("black")]
-     }
-  end
-
-  def fill_gamepieces
-    @gamepieces[:white].each_value do |value|
-      value.each do |piece|
-        change_board(piece.location, piece.icon)
-      end
-    end
-
-    @gamepieces[:black].each_value do |value|
-      value.each do |piece|
-        change_board(piece.location, piece.icon)
-      end
-    end
-  end
-  
+ 
   def change_board(coordinates, element)
     file = find_file(coordinates[0])
     rank = find_rank(coordinates[1])
--- chess/lib/game_pieces/knight.rb
+++ chess/lib/game_pieces/knight.rb
@@ -7,4 +7,29 @@ class Knight
     @location = location
     @icon = @color == "white" ? "\u2658" : "\u265E"
   end
+
+  def moves(location, moves = [])
+    knight_directions = [[-2, -1], [-2, 1], [2, 1], [2,-1], [-1, 2], [-1, -2], [1, 2], [1, -2]]
+    
+    knight_directions.each do |direction|
+      row = location[0] + direction[0]
+      col = location[1] + direction[1]
+      next if invalid_move?([row, col])
+      direction = [row, col]
+      moves << direction
+    end
+
+    moves
+  end
+
+  def invalid_move?(move)
+    if move[0] < 1 || move[0] > 8
+      return true
+    elsif move[1] < 1 || move[1] > 8
+      return true
+    else
+      return false
+    end
+  end
+
 end
