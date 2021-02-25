fd35784ddb2e93e7b2131ba43d53e9a4153be7a4
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-02-25 01:40:12 -0500

    Implemented King Movement.
    Movement almost finished: Need En passant and Castling.

--- chess/lib/game_pieces/king.rb
+++ chess/lib/game_pieces/king.rb
@@ -1,16 +1,17 @@
 class King
-  attr_reader :icon, :color, :location
+  attr_reader :icon, :color, :location, :name
   
   def initialize(color)
+    @name = "K"
     @color = color
     @location = @color == "white" ? "e1" : "e8"
     @icon = @color == "white" ? "\u2654" : "\u265A"
   end
 
   def moves(location, moves = [])
-    knight_directions = [[-2, -1], [-2, 1], [2, 1], [2,-1], [-1, 2], [-1, -2], [1, 2], [1, -2]]
+    king_directions = [[-1, -1], [-1, 1], [1, 1], [1,-1], [-1, 0], [1, 0], [0, -1], [0, 1]]
     
-    knight_directions.each do |direction|
+    king_directions.each do |direction|
       row = location[0] + direction[0]
       col = location[1] + direction[1]
       next if invalid_move?([row, col])
@@ -19,6 +20,16 @@ class King
     end
 
     moves
+  end
+
+  def invalid_move?(move)
+    if move[0] < 1 || move[0] > 8
+      return true
+    elsif move[1] < 1 || move[1] > 8
+      return true
+    else
+      return false
+    end
   end
 
   def change_location(coordinates)
