d99892b273ea03fa732ad86bb092bdc143c4ca75
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-03-26 21:16:24 -0400

    Add files via upload

--- maze_solver/lib/maze.rb
+++ maze_solver/lib/maze.rb
@@ -25,43 +25,111 @@ class Maze
   end
 
   #Starting at S, move only when the space is a space character, and find the E.
-  #Using recursion, we just need one method. 
+  # Recursion: (confused)
+    # run navigate_maze recursively until you run into E. 
+    # each time you move, you would push into an array(this keeps track of your previous movements.)
+    # if you run into a wall, then you would return.
+      # this array would be useless.
+    # if you run into E, then you would return the array, this is our path that we are looking for.
+
+  # Iteratively:
+    # Until you run into "E"
+      # go in each direction
+      # add that direction to an array
+      # if you run into a "*", break
+  
+  # Alternatively:
+    # Loop
+      # Go in a direction
+        # if you run into a *, need to go a different direction
+        # if you run into a " ", change new_location to current location
+          # Mark old location as an x
+        # if you run into an x, you need to go in a different direction
+
+  # Problem:
+    # Loop needs to backtrack to where other moves are available, and make those other moves. 
+      # How will we tackle the backtrack to doing other moves when there are no other other available at this spot.
+        # Add the moves to an array, if you run into a dead end, change the current_location to the previous one; pop the array. 
+
   def navigate_maze(current_location)
-    return if current_location == "*"
+    movements = []
 
     row = current_location[0]
     col = current_location[1]
 
-    go_north([])
-  end
+    if current_location == "E"
+      return "At the end."
+    end
 
-  def check_spot
-    
+    if go_north(current_location)
+      @maze[row][col] = "x"
+      current_location = [row - 1, col]
+      navigate_maze(current_location)
+      movements << current_location
+    elsif go_south(current_location)
+      @maze[row][col] = "x"
+      current_location = [row + 1, col]
+      navigate_maze(current_location)
+      movements << current_location
+    elsif go_east(current_location)
+      @maze[row][col] = "x"
+      current_location = [row, col + 1]
+      navigate_maze(current_location)
+      movements << current_location
+    elsif go_west(current_location)
+      @maze[row][col] = "x"
+      current_location = [row, col - 1]
+      navigate_maze(current_location)
+      movements << current_location
+    else 
+      
+    end
+
   end
 
   #takes in current location as coordinates; ex: [0,0]
   def go_north(coordinates)
     row = coordinates[0]
     col = coordinates[1]
-    return [row - 1, col]
+
+    if self[row - 1, col] == " "
+      return true
+    else
+      return false
+    end
   end
 
   def go_south(coordinates)
     row = coordinates[0]
     col = coordinates[1]
-    return [row + 1, col]
+
+    if self[row + 1, col] == " "
+      return true
+    else
+      return false
+    end
   end
 
   def go_east(coordinates)
     row = coordinates[0]
     col = coordinates[1]
-    return [row, col + 1]
+
+    if self[row, col + 1] == " "
+      return true
+    else
+      return false
+    end
   end
 
   def go_west(coordinates)
     row = coordinates[0]
     col = coordinates[1]
-    return [row, col - 1]
+
+    if self[row, col - 1] == " "
+      return true
+    else
+      return false
+    end
   end
 
   def [](row, col)
